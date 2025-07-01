8ddc9c242b810ce6532275084e2ddb3bb247e9e6
sxxntoz <2201010219@undc.edu.pe>
2025-07-01 10:59:34 -0500

    imprimir pos

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -34,12 +34,9 @@ $detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Boleta</title>
-  <!-- Enlazar el archivo CSS de la boleta -->
+  <!-- Enlazar el CSS de la boleta -->
   <link rel="stylesheet" href="../assets/css/boleta_pos.css">
   <link rel="stylesheet" href="../assets/css/dashboard.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
 <body>
   <div class="boleta-box">
     <!-- Encabezado -->
