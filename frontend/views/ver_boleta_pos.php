caddc4c81c7fc63177f25171240062004c77ed2c
sxxntoz <2201010219@undc.edu.pe>
2025-07-01 11:36:06 -0500

    cambios

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -35,7 +35,9 @@ $detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Boleta</title>
   <!-- Enlazar el CSS de la boleta -->
-  <link rel="stylesheet" href="../assets/css/boleta_pos.css">  
+  <link rel="stylesheet" href="../assets/css/boleta_pos.css">
+  <link rel="stylesheet" href="../assets/css/dashboard.css">
+
 </head>
 <body>
   <div class="boleta-box">
