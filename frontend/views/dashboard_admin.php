6de99c1ef9e6a7e5617bc1dae6013f6321d82ad8
sxxntoz <2201010219@undc.edu.pe>
2025-07-01 09:29:57 -0500

    cambios guardados eliminacion de footer

--- frontend/views/dashboard_admin.php
+++ frontend/views/dashboard_admin.php
@@ -27,8 +27,7 @@ $ventasHoy = $conn->query("SELECT COALESCE(SUM(total), 0) FROM ventas WHERE DATE
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Administrador</title>
-  <link href="../assets/css/footer.css" rel="stylesheet" />
+  <title>Administrador</title>  
   <link rel="stylesheet" href="../assets/css/dashboard.css">  
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
@@ -80,8 +79,5 @@ $ventasHoy = $conn->query("SELECT COALESCE(SUM(total), 0) FROM ventas WHERE DATE
 <script src="../js/dashboard_administrador.js"></script>
 
 </body>
-<footer class="footer">
-  <p>© 2025 Botica Bienestar y Salud. Todos los derechos reservados.</p>
-</footer>
 </html>
 
