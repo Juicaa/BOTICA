af4a74650e7a408d743629542909037252dfd9ee
sxxntoz <2201010219@undc.edu.pe>
2025-07-01 09:39:37 -0500

    prueba de menu lateral funcionando

--- frontend/assets/js/menu_lateral.js
+++ frontend/assets/js/menu_lateral.js
@@ -1,4 +1,7 @@
+// frontend/js/menu_lateral.js
+
 function toggleMenu() {
-  var menu = document.getElementById('menuLateral');
-  menu.classList.toggle('active'); // Alternar la clase 'active' para mostrar u ocultar el menú
+    var menu = document.getElementById('menuLateral');
+    console.log(menu.classList);  // Esto es para verificar si la función se ejecuta
+    menu.classList.toggle('active'); // Alternar la clase 'active' para mostrar u ocultar el menú
 }
