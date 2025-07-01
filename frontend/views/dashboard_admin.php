bd6af4fb7e98c039ec085e449b4afc1f13e68d05
sxxntoz <2201010219@undc.edu.pe>
2025-07-01 08:58:07 -0500

    cambio en css

--- frontend/views/dashboard_admin.php
+++ frontend/views/dashboard_admin.php
@@ -28,8 +28,8 @@ $ventasHoy = $conn->query("SELECT COALESCE(SUM(total), 0) FROM ventas WHERE DATE
 <head>
   <meta charset="UTF-8">
   <title>Administrador</title>
-  <link rel="stylesheet" href="../assets/css/dashboard.css">
   <link href="../assets/css/footer.css" rel="stylesheet" />
+  <link rel="stylesheet" href="../assets/css/dashboard.css">  
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
