b87350af5f93906c1dcae4999657b102d6613b74
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 17:21:43 -0500

    queda

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -47,9 +47,8 @@ $stmt_productos->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boleta de Compra</title>
     <link rel="stylesheet" href="../assets/css/boleta_pos.css"> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="ticket">
