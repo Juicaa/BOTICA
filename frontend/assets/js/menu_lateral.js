6181e1ca7511b99a3894f15be6be6f447b94f5fe
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 14:32:45 -0500

    menu_lateral.js

--- frontend/assets/js/menu_lateral.js
+++ frontend/assets/js/menu_lateral.js
@@ -1,6 +1,6 @@
-// frontend/js/menu_lateral.js
-
 function toggleMenu() {
     var menu = document.getElementById('menuLateral');
+    var contenido = document.getElementById('contenidoPrincipal'); // Asegúrate de que el ID del contenido sea correcto
     menu.classList.toggle('active'); // Alternar la clase 'active' para mostrar u ocultar el menú
+    contenido.classList.toggle('active'); // Alternar la clase 'active' para mover el contenido
 }
