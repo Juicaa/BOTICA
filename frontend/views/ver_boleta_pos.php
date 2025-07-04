6f77e300cfd0db39edf242592fc14102a7e3c206
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 20:59:37 -0500

    boleta

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -34,12 +34,12 @@ $detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <title>Boleta</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/dashboard.css"> 
-  <link rel="stylesheet" href="../assets/css/boleta_pos.css"> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <style>
+    <link rel="stylesheet" href="../assets/css/boleta_pos.css"> 
+    <link rel="stylesheet" href="../assets/css/dashboard.css">   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
     body {
       font-family: monospace;
       font-size: 12px;
