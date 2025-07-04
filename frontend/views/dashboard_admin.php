d4fe2f98213dd85e9a4183440aaff2186ead8d8e
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 22:22:44 -0500

    update dashboard_admin.php

--- frontend/views/dashboard_admin.php
+++ frontend/views/dashboard_admin.php
@@ -29,10 +29,10 @@ $ventasHoy = $conn->query("SELECT COALESCE(SUM(total), 0) FROM ventas WHERE DATE
   <meta charset="UTF-8">
   <title>Administrador</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/dashboard.css"> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/dashboard.css">   
+  
   
 
 </head>
