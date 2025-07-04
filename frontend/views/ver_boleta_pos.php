bfe691e0843b2ff7deb82b61a4d8840a8d34a8dd
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 21:06:11 -0500

    cambios boleta

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -34,8 +34,7 @@ $detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <title>Boleta</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/boleta_pos.css"> 
-  <link rel="stylesheet" href="../assets/css/dashboard.css">   
+  <link rel="stylesheet" href="../assets/css/boleta_pos.css">   
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 </head>
