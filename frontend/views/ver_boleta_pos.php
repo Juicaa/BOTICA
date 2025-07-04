c93d03bda493507c97dbffb230026f0ad01b5050
sxxntoz <2201010219@undc.edu.pe>
2025-07-03 21:30:45 -0500

    cambio

--- frontend/views/ver_boleta_pos.php
+++ frontend/views/ver_boleta_pos.php
@@ -39,7 +39,7 @@ $detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <style>
+  <style>
     body {
       font-family: monospace;
       font-size: 12px;
